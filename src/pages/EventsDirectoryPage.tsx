04b97ec387bd772d616c31990b54229744e05562
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2025-09-30 16:09:18 -0500

    Fix white screen issue by adjusting layout heights

--- src/pages/EventsDirectoryPage.tsx
+++ src/pages/EventsDirectoryPage.tsx
@@ -305,7 +305,6 @@ export const EventsDirectoryPage: React.FC = () => {
         )}
 
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 lg:py-8">
-        <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 lg:py-8">
           {/* Desktop Header */}
           <div className="hidden lg:block mb-8">
             <div className="flex items-start justify-between mb-6">
@@ -440,9 +439,9 @@ export const EventsDirectoryPage: React.FC = () => {
               
               {/* Desktop Layout - Split Screen */}
               <div className="hidden lg:block">
-                <div className="grid grid-cols-3 gap-8 h-[calc(100vh-300px)]">
+                <div className="grid grid-cols-3 gap-8 min-h-[600px]">
                   {/* Left Panel - Event List */}
-                  <div className="col-span-2 overflow-y-auto">
+                  <div className="col-span-2 overflow-y-auto max-h-[80vh]">
                     <div className="grid grid-cols-2 gap-6 pr-6">
                       {filteredEvents.map((event) => (
                         <div
@@ -463,7 +462,7 @@ export const EventsDirectoryPage: React.FC = () => {
                   </div>
                   
                   {/* Right Panel - Event Details */}
-                  <div className="col-span-1 overflow-y-auto">
+                  <div className="col-span-1 overflow-y-auto max-h-[80vh]">
                     <EventDetailPanel eventSlug={selectedEventSlug} />
                   </div>
                 </div>
