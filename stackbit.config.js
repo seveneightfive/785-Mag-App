3da3dbde1b495ee1dfc5f132591f084cc3af9dd6
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-01-04 21:00:23 -0600

    Migrate stackbit.config.js to TypeScript format

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,4 +1,23 @@
-export default {
-  stackbitVersion: '~0.6.0',
-  ssgName: 'nextjs',
-}
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
+});
